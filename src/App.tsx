e6f86d2c4b6a9318b10546b98d4b4e984399a0fa
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2025-06-02 10:00:00 +0530

    init react

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Blog from './components/Blog';
+import BlogPost from './components/BlogPost';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
